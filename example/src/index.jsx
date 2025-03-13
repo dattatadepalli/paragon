5c2d75457500a10912ff4cb34bbbfb6480047ad9
Adam Stankiewicz <agstanki@gmail.com>
2025-03-13 12:55:33 -0400

    chore: replicate selection bug with controlled table selections

--- example/src/index.jsx
+++ example/src/index.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import {
   AppProvider,
   ErrorPage,
-  PageRoute,
+  PageWrap,
 } from '@edx/frontend-platform/react';
 import { APP_INIT_ERROR, APP_READY, initialize } from '@edx/frontend-platform';
+import { Routes, Route } from 'react-router-dom';
 import { subscribe } from '@edx/frontend-platform/pubSub';
 import MyComponent from './MyComponent';
 
 import './index.scss';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 subscribe(APP_READY, () => {
-  ReactDOM.render(
+  root.render(
     <AppProvider>
-      <PageRoute
-        exact
-        path="/"
-        component={MyComponent}
-      />
-    </AppProvider>,
-    document.getElementById('root'),
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<PageWrap><MyComponent /></PageWrap>}
+        />
+      </Routes>
+    </AppProvider>
   );
 });
 
 subscribe(APP_INIT_ERROR, (error) => {
-  ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+  root.render(<ErrorPage message={error.message} />);
 });
 
 initialize({
